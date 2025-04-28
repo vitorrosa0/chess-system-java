00bfff900b4a0f5a0cb4e4d25edc87f476a5893c
vitorrosa0 <vrfrosa1@gmail.com>
2025-04-28 19:35:15 -0300

    Project created

---
+++ Chess-System/src/main/java/com/chess/Chess_System/ChessSystemApplication.java
@@ -0,0 +1,13 @@
+package com.chess.Chess_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessSystemApplication.class, args);
+	}
+
+}
---
+++ Chess-System/src/test/java/com/chess/Chess_System/ChessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chess.Chess_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
