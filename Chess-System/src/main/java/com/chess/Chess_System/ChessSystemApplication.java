9f922f53038fa5b8d636320a3216e8ed11ff394f
vitorrosa0 <vrfrosa1@gmail.com>
2025-05-08 12:11:39 -0300

    Handling captured pieces

--- Chess-System/src/main/java/com/chess/Chess_System/ChessSystemApplication.java
+++ Chess-System/src/main/java/com/chess/Chess_System/ChessSystemApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -23,11 +25,12 @@ public class ChessSystemApplication implements CommandLineRunner {
 
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -40,6 +43,10 @@ public class ChessSystemApplication implements CommandLineRunner {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+				if (capturedPiece != null){
+					captured.add(capturedPiece);
+				}
 			} catch (ChessException | InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
