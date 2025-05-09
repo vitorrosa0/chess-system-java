95c4269562aedb90ebc2168a0687848b80f86e92
vitorrosa0 <vrfrosa1@gmail.com>
2025-05-09 12:46:01 -0300

    Bishop

--- Chess-System/src/main/java/com/chess/Chess_System/chess/ChessMatch.java
+++ Chess-System/src/main/java/com/chess/Chess_System/chess/ChessMatch.java
@@ -3,6 +3,7 @@ package com.chess.Chess_System.chess;
 import com.chess.Chess_System.boardgame.Board;
 import com.chess.Chess_System.boardgame.Piece;
 import com.chess.Chess_System.boardgame.Position;
+import com.chess.Chess_System.chess.pieces.Bishop;
 import com.chess.Chess_System.chess.pieces.King;
 import com.chess.Chess_System.chess.pieces.Pawn;
 import com.chess.Chess_System.chess.pieces.Rook;
@@ -201,6 +202,8 @@ public class ChessMatch {
         placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
@@ -213,6 +216,8 @@ public class ChessMatch {
         placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 
     }
 }
---
+++ Chess-System/src/main/java/com/chess/Chess_System/chess/pieces/Bishop.java
@@ -0,0 +1,66 @@
+package com.chess.Chess_System.chess.pieces;
+
+import com.chess.Chess_System.boardgame.Board;
+import com.chess.Chess_System.boardgame.Position;
+import com.chess.Chess_System.chess.ChessPiece;
+import com.chess.Chess_System.chess.Color;
+
+public class Bishop extends ChessPiece {
+
+    public Bishop(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        // diagonal up right
+        p.setValues(position.getRow() - 1, position.getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() - 1, p.getColumn() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //diagonal up left
+        p.setValues(position.getRow() - 1, position.getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() - 1, p.getColumn() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // diagonal down left
+        p.setValues(position.getRow() + 1, position.getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() + 1, p.getColumn() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // diagonal down right
+        p.setValues(position.getRow() + 1, position.getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() + 1, p.getColumn() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
+    }
+    @Override
+    public String toString() {
+        return "B";
+    }
+}
